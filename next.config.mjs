9f96c6f56825588e38301b039236bc9d846c66da
Alishba Moin <159716625+Alishba-Moin@users.noreply.github.com>
2024-08-29 21:52:16 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    env: {
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        GITHUB_ID: process.env.GITHUB_ID,
+        GITHUB_SECRET: process.env.GITHUB_SECRET,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+      },
 };
 
 export default nextConfig;
